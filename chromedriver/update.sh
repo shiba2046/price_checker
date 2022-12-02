76f084b22d8e0deab8a81e4ff3d2572501d07bc4
shiba2046 <17551496+shiba2046@users.noreply.github.com>
2022-12-02 15:16:00 +0800

    Added webdriver update check

---
+++ chromedriver/update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+version=$(chromium-browser --version | cut -d ' ' -f2 | xargs)
+# debug
+# version='108.0.5359.71' 
+existing_version=$(cat linux-version | xargs)
+
+echo Driver: $existing_version
+echo Chrome: $version
+
+if [[ $version == $existing_version ]]; then
+    echo No update needed.
+else
+    url="https://chromedriver.storage.googleapis.com/$version/chromedriver_linux64.zip"
+    echo Download new driver $url
+    curl -o chromedriver.zip $url 
+    unzip -o chromedriver.zip
+    if [[ $? -eq 0 ]]; then 
+        echo Driver updated to $version
+        echo $version > linux-version
+        rm -rf *zip*
+    else
+        echo There was something wrong when updating. Exit.
+    fi
+    
+fi
