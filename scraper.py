b7aeee3515c4866ffeb6f49f986e48ec3c743f70
shiba2046 <17551496+shiba2046@users.noreply.github.com>
2022-12-02 13:35:38 +0800

    Fix chromium version

--- scraper.py
+++ scraper.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.utils import ChromeType, read_version_from_cmd, PATTERN
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import os
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+if "GITHUB_ACTION" in os.environ: 
+    version = read_version_from_cmd("/usr/bin/chromium-browser --version", PATTERN["Chromium"])
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM, version=version, path='/usr/bin/chromium-browser') .install())
+else:
+    chrome_service = Service(ChromeDriverManager().install())
 
 chrome_options = Options()
 options = [
@@ -19,13 +24,19 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
+chrome_options.experimental_options["prefs"] = {
+    "profile.default_content_settings": {"images": 2},
+    "profile.managed_default_content_settings" : {"images": 2},
+    
+}
+chrome_options.add_experimental_option("excludeSwitches", ['enable-logging'])
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 # driver.get('http://nytimes.com')
 # print(driver.title)
 
 from jd_crawler import jd_crawler
-import pandas as pd
+# import pandas as pd
 
 jd = jd_crawler(driver)
 with open('jd_list.txt', 'r') as f:
@@ -33,5 +44,9 @@ with open('jd_list.txt', 'r') as f:
 
 results = jd.query_skus(jd_list)
 
-df = pd.DataFrame.from_records(results)
-df.to_csv('jd_results.csv', float_format='%.2f', index=False, encoding='utf-8-sig')
+# df = pd.DataFrame.from_records(results)
+# df.to_csv('jd_results.csv', float_format='%.2f', index=False, encoding='utf-8-sig')
+from csv import DictWriter
+with open("results/jd_results.csv", 'a', encoding='utf-8-sig') as csv_file:
+    writer_object = DictWriter(csv_file, fieldnames=results[0].keys())
+    writer_object.writerows(results) 
